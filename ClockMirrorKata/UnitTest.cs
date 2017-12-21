0a4d3a20f936d627f16c2c7ab13238d4974345b5
katrina <inlove0131@hotmail.com>
2017-12-21 20:22:59 +0800

    refactor

--- ClockMirrorKata/UnitTest.cs
+++ ClockMirrorKata/UnitTest.cs
@@ -13,7 +13,7 @@ namespace ClockMirrorKata
         public void Hour12Return11()
         {
             var expected = "11";
-            var actual = ClockMirror.hour(12);
+            var actual = ClockMirror.Hour(12, 1);
             Assert.AreEqual(expected, actual);
         }
 
@@ -21,7 +21,7 @@ namespace ClockMirrorKata
         public void Hour3Return8()
         {
             var expected = "08";
-            var actual = ClockMirror.hour(3);
+            var actual = ClockMirror.Hour(3, 1);
             Assert.AreEqual(expected, actual);
         }
 
@@ -29,7 +29,7 @@ namespace ClockMirrorKata
         public void Minute10Return50()
         {
             var expected = "50";
-            var actual = ClockMirror.minutes(10);
+            var actual = ClockMirror.Minutes(10);
             Assert.AreEqual(expected, actual);
         }
 
@@ -37,7 +37,7 @@ namespace ClockMirrorKata
         public void Minute0Return0()
         {
             var expected = "00";
-            var actual = ClockMirror.minutes(0);
+            var actual = ClockMirror.Minutes(0);
             Assert.AreEqual(expected, actual);
         }
 
@@ -50,10 +50,26 @@ namespace ClockMirrorKata
         }
 
         [TestMethod]
+        public void Time1101te0Return1159()
+        {
+            var expected = "01:01";
+            var actual = ClockMirror.FormatTime("10:59");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
         public void Time311Return0949()
         {
             var expected = "08:49";
             var actual = ClockMirror.FormatTime("03:11");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Time300Return0900()
+        {
+            var expected = "09:00";
+            var actual = ClockMirror.FormatTime("03:00");
             Assert.AreEqual(expected, actual);
         }
 
@@ -72,30 +88,44 @@ namespace ClockMirrorKata
         {
             var h = Convert.ToInt32(time.Split(':')[0]);
             var min = Convert.ToInt32(time.Split(':')[1]);
-            string hour = ClockMirror.hour(h, min);
-            string minutes = ClockMirror.minutes(min);
+            var hour = ClockMirror.Hour(h, min);
+            var minutes = ClockMirror.Minutes(min);
 
             return $"{hour}:{minutes}";
         }
 
-        public static string hour(int h, int minute = 01)
+        public static string Hour(int h, int minute)
         {
-            if (h == 12 && minute == 00)
+            if (h == 12 && minute == 0)
             {
                 return "12";
             }
-            if (h == 12)
-                return "11";
-            if (h == 11)
-                return "12";
-            return $"{0}{11 - h}";
+
+            if (h < 3 && minute == 0)
+            {
+                return $"{12 - h}";
+            }
+
+            if (minute == 0)
+            {
+                return $"{0}{12 - h}";
+            }
+
+            if (h > 10) return $"{23 - h}";
+
+            if (h > 1) return $"{0}{11 - h}";
+
+            return $"{11 - h}";
         }
 
-        public static string minutes(int min)
+        public static string Minutes(int min)
         {
-            if (min == 0)
-                return "00";
-            return (60-min).ToString();
+            if (min == 0) return "00";
+            if (min > 50)
+            {
+                return $"{0}{(60 - min).ToString()}";
+            }
+            return (60 - min).ToString();
         }
-}
     }
+}
